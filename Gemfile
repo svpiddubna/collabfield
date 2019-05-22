303c7599e02cf93f580df926db3dc47c218e701a
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-05-22 14:38:38 +0300

    Add specs to NavigationHelper's collapsible_links_partial_path method

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
 end
 
 group :development do
@@ -64,7 +65,7 @@ group :test do
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
   gem 'rspec-rails', '~> 3.6'
-  gem 'factory_girl_rails'
+  gem "factory_bot"
   gem 'rails-controller-testing'
   gem 'headless'
   gem 'poltergeist'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,11 +9,10 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/poltergeist'
-require 'factory_girl_rails'
+require 'factory_bot'
 require 'capybara/rspec'
 
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::Methods
+
 Capybara.javascript_driver = :poltergeist
 Capybara.server = :puma
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -86,4 +85,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
 end
