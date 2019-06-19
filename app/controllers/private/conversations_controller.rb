4304cc9daafc52d68b1234bf7ff219856adc2c4d
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-06-19 15:38:19 +0200

    Inside a post add a form to contact a user

---
+++ app/controllers/private/conversations_controller.rb
@@ -0,0 +1,31 @@
+class Private::ConversationsController < ApplicationController
+  def show
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
+  end
+
+  private
+
+  def conversation_exist?
+    Private::Conversation.between_users(current_user.id, @post.user.id).present?
+  end
+
+  def create
+    recipient_id = Post.find(params[:post_id]).user.id
+    conversation = Private::Conversation.new(sender_id: current_user.id,
+                                             recipient_id: recipient_id)
+    if conversation.save
+      Private::Message.create(user_id: recipient_id,
+                              conversation_id: conversation.id,
+                              body: params[:message_body])
+      respond_to do |format|
+        format.js {render partial: 'posts/show/contact_user/message_form/success'}
+      end
+    else
+      respond_to do |format|
+        format.js {render partial: 'posts/show/contact_user/message_form/fail'}
+      end
+    end
+  end
+end
